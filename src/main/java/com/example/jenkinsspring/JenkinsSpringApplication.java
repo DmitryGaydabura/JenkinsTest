d635a9e83d3846ad5d0efcaa5e3a417d66dc9574
Dmitry Gaydabura <gaydabura.d@icloud.com>
2024-09-05 17:33:05 +0300

    Sixth commit

--- src/main/java/com/example/jenkinsspring/JenkinsSpringApplication.java
+++ src/main/java/com/example/jenkinsspring/JenkinsSpringApplication.java
@@ -9,10 +9,7 @@ public class JenkinsSpringApplication {
   public static void main(String[] args) {
     SpringApplication.run(JenkinsSpringApplication.class, args);
     System.out.println(123);
-    System.out.println(123);
-    System.out.println(123);
-    System.out.println(123);
-    System.out.println(123);
+
   }
 
 }
