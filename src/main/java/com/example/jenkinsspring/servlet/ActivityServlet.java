80169569157dac0cbaa56f107c30a7628055587d
Dmitry Gaydabura <gaydabura.d@icloud.com>
2024-09-18 15:17:11 +0300

    HotFix

--- src/main/java/com/example/jenkinsspring/servlet/ActivityServlet.java
+++ src/main/java/com/example/jenkinsspring/servlet/ActivityServlet.java
@@ -1,17 +1,20 @@
 package com.example.jenkinsspring.servlet;
 
 
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.sql.*;
-import java.util.ArrayList;
-import java.util.List;
-import java.io.IOException;
 
-@WebServlet("/activity")
+
 public class ActivityServlet extends HttpServlet {
   private Connection connection;
 
