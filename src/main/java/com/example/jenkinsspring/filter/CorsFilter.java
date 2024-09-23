6c4a7bc3c0350e762c98f315aab3133cec460dfe
Dmitry Gaydabura <gaydabura.d@icloud.com>
2024-09-23 12:09:10 +0300

    Fix

--- src/main/java/com/example/jenkinsspring/filter/CorsFilter.java
+++ src/main/java/com/example/jenkinsspring/filter/CorsFilter.java
@@ -1,40 +1,23 @@
 package com.example.jenkinsspring.filter;
 
 import javax.servlet.*;
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 public class CorsFilter implements Filter {
 
   @Override
-  public void init(FilterConfig filterConfig) throws ServletException {
+  public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+    HttpServletResponse response = (HttpServletResponse) res;
+    response.setHeader("Access-Control-Allow-Origin", "*");
+    response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PUT");
+    response.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    chain.doFilter(req, res);
   }
 
   @Override
-  public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
-      throws IOException, ServletException {
-
-    HttpServletResponse res = (HttpServletResponse) response;
-    HttpServletRequest req = (HttpServletRequest) request;
-
-    // Устанавливаем заголовки для разрешения CORS
-    res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
-    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-    res.setHeader("Access-Control-Allow-Headers", "Origin, Content-Type, Accept");
-    res.setHeader("Access-Control-Allow-Credentials", "true");
-
-    // Для запросов типа OPTIONS сразу возвращаем 200 OK
-    if ("OPTIONS".equalsIgnoreCase(req.getMethod())) {
-      res.setStatus(HttpServletResponse.SC_OK);
-      return;
-    }
-
-    chain.doFilter(request, response);
-  }
+  public void init(FilterConfig filterConfig) {}
 
   @Override
-  public void destroy() {
-  }
+  public void destroy() {}
 }
-
