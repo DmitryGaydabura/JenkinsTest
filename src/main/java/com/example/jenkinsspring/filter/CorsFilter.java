b3bd09cf56c876fff69900924a984ee1ed427f33
Dmitry Gaydabura <gaydabura.d@icloud.com>
2024-09-25 11:30:47 +0300

    Fix Postman Endpoints

--- src/main/java/com/example/jenkinsspring/filter/CorsFilter.java
+++ src/main/java/com/example/jenkinsspring/filter/CorsFilter.java
@@ -1,6 +1,7 @@
 package com.example.jenkinsspring.filter;
 
 import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
@@ -8,15 +9,27 @@ public class CorsFilter implements Filter {
 
   @Override
   public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+    HttpServletRequest request = (HttpServletRequest) req;
     HttpServletResponse response = (HttpServletResponse) res;
-    response.setHeader("Access-Control-Allow-Origin", "*");
+
+    // Разрешаем все домены (*), можно ограничить конкретными доменами
+    response.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");  // Замените на нужный домен или оставьте "*", если не требуется куки
     response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PUT");
+    response.setHeader("Access-Control-Max-Age", "3600"); // Кэширование preflight-запросов
     response.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+
+
+    // Обрабатываем preflight OPTIONS-запросы
+    if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
+      response.setStatus(HttpServletResponse.SC_OK); // Возвращаем 200 OK для preflight-запросов
+      return;
+    }
+
     chain.doFilter(req, res);
   }
 
   @Override
-  public void init(FilterConfig filterConfig) {}
+  public void init(FilterConfig filterConfig) throws ServletException {}
 
   @Override
   public void destroy() {}
